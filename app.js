4bcb2e8ceeb337d9afdc85c2373546408115dcbf
Alejandro Salgado G <asalgad2@eafit.edu.co>
2017-07-23 21:36:36 -0500

    Isolationg the routes logic

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
+var routes = require('./routes');
+
 // Routes
-app.get('/', function(req, res){
-    res.render('home');
-});
+app.get('/', routes.home);
 
 app.listen(3000, function(){
     console.log("The application is running on localhost:3000");
